ea2c9b3232a5ebe292a2e4039286474bfd54ded2
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-03-31 12:57:03 +0100

    dark

--- resources/views/boards/clients_board.blade.php
+++ resources/views/boards/clients_board.blade.php
@@ -21,34 +21,34 @@
 
                                 <!-- Dropdown menu -->
                                 <div id="dropdown"
-                                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
+                                    <ul class="py-2 text-sm text-gray-700"
                                         aria-labelledby="dropdownDefaultButton">
                                         <li>
                                             <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white border-b-slate-400 border-b">My
+                                                class="block px-4 py-2 hover:bg-gray-100 border-b-slate-400 border-b">My
                                                 plan</a>
                                         </li>
                                         <li>
                                             <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white border-b-slate-400 border-b">Account</a>
+                                                class="block px-4 py-2 hover:bg-gray-100 border-b-slate-400 border-b">Account</a>
                                         </li>
                                         <li>
                                             <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white border-b-slate-400 border-b">Getting
+                                                 class="block px-4 py-2 hover:bg-gray-100 border-b-slate-400 border-b">Getting
                                                 started</a>
                                         </li>
                                         <li>
                                             <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white border-b-slate-400 border-b">Documentation</a>
+                                                class="block px-4 py-2 hover:bg-gray-100 border-b-slate-400 border-b">Documentation</a>
                                         </li>
                                         <li>
                                             <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white border-b-slate-400 border-b">Support</a>
+                                                class="block px-4 py-2 hover:bg-gray-100 border-b-slate-400 border-b">Support</a>
                                         </li>
                                         <li>
                                             <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign
+                                                class="block px-4 py-2 hover:bg-gray-100">Sign
                                                 out</a>
                                         </li>
                                     </ul>
@@ -61,8 +61,8 @@
 
                                 <!-- Dropdown menu -->
                                 <div id="dropdown1"
-                                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-48 dark:bg-gray-700">
-                                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-48">
+                                    <ul class="py-2 text-sm text-gray-700"
                                         aria-labelledby="dropdownDefaultButton">
                                         <li>
                                             <div class="block px-4 py-2 text-[#999999] text-sm">
@@ -95,10 +95,10 @@
                         <form class="w-1/2">
                             <div class="relative max-w-sm">
                                 <label for="countries"
-                                    class="absolute top-1 left-4 block mb-2 text-xs font-medium text-gray-900 dark:text-white">Select
+                                    class="absolute top-1 left-4 block mb-2 text-xs font-medium text-gray-900">Select
                                     API Key</label>
                                 <select id="countries"
-                                    class="bg-gray-50 border border-blue-500 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-4 pt-5 pb-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 font-semibold">
+                                    class="bg-gray-50 border border-blue-500 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-4 pt-5 pb-2 font-semibold">
                                     <option>t-65...b62d - Mainnet API key</option>
                                     <option>t-65...b62d - Mainnet API key</option>
                                 </select>
@@ -111,9 +111,9 @@
                         </div>
                     </div>
                     <div class="relative overflow-x-auto">
-                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500">
                             <thead
-                                class="text-sm text-gray-400 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border">
+                                class="text-sm text-gray-400 uppercase bg-gray-50 border">
                                 <tr>
                                     <th scope="col" class="px-6 py-3">
                                         S/N
@@ -144,8 +144,8 @@
                             @if(count($tableArray) > 0)
                            <tbody>
                             @foreach($tableArray as $index => $table)
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 whitespace-wrap">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <tr class="bg-white border-b whitespace-wrap">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                     {{ $index + 1 }}
                                 </th>
                                 <td class="px-6 py-4 break-all">
@@ -177,7 +177,7 @@
                         @if(count($tableArray) < 0)
                         <div>
                             <div
-                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 relative p-10 flex justify-center items-center text-sm text-gray-400">
+                                class="bg-white border-b relative p-10 flex justify-center items-center text-sm text-gray-400">
                                 <p>No clients available. Create a client to start sending notifications.</p>
                             </div>
                         </div>
@@ -193,13 +193,13 @@
 <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div class="relative bg-white rounded-lg shadow">
             <!-- Modal header -->
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+                <h3 class="text-xl font-semibold text-gray-900">
                     Create Client
                 </h3>
-                <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
+                <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="authentication-modal">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                     </svg>
@@ -210,22 +210,22 @@
             <div class="p-4 md:p-5">
                 <form class="space-y-4" action="#">
                     <div>
-                        <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
-                        <input type="text" name="firstname" id="firstname" placeholder="Enter the first name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                        <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900">First Name</label>
+                        <input type="text" name="firstname" id="firstname" placeholder="Enter the first name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
                     </div>
                     <div>
-                        <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
-                        <input type="text" name="lastname" id="lastname" placeholder="Enter the last name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                        <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900">Last Name</label>
+                        <input type="text" name="lastname" id="lastname" placeholder="Enter the last name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
                     </div>
                     <div>
-                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Address</label>
-                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email Address</label>
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@company.com" required>
                     </div>
                     <div>
-                        <label for="schoolname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">School Name</label>
-                        <input type="text" name="schoolname" id="schoolname" placeholder="Enter the school name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                        <label for="schoolname" class="block mb-2 text-sm font-medium text-gray-900">School Name</label>
+                        <input type="text" name="schoolname" id="schoolname" placeholder="Enter the school name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
                     </div>
-                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Client</button>
+                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create Client</button>
                 </form>
             </div>
         </div>
